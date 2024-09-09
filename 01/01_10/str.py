c49a1df5b6fdd787263f2325a48f6860f4e68615
BlacKnife123 <piotrwn106@gmail.com>
2024-09-09 16:57:19 +0200

    koniec 1 sekcji

--- 01/01_10/str.py
+++ 01/01_10/str.py
@@ -2,3 +2,16 @@
 
 #Skrypt do lekcji 01_10 String - zmienne łańcuchowe
 
+newstring = "Witaj \n w kursie"
+newstring2 = "Ha"
+newstring3 = "Tab \t - \" '"
+newstring4 = '\'"cos"'
+
+#print(type(newstring))
+#print(newstring)
+#print(newstring2 * 4)
+
+
+print(newstring.lower())
+print(newstring.find('k'))
+print(newstring3 + " " + newstring4)
